c18e62365b56d2085a3b96e6b2eb07cf9116c5fa
Kwas <ak12@spoko.pl>
2024-04-13 13:19:25 +0200

    add task part one trebuchet

---
+++ trebuchet/trebuchet-p1/src/calculateCalibrationFromLine.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import readline from "readline";
+
+function calculateCalibrationFromLine({ line }: { line: string }) {
+  let firstNumber = null;
+  let secondNumber = null;
+
+  for (const char of line) {
+    const candidateToNumber = Number(char);
+    if (Number.isInteger(candidateToNumber)) {
+      if (firstNumber == null) {
+        firstNumber = candidateToNumber;
+      }
+      secondNumber = candidateToNumber;
+    }
+  }
+  return Number(`${firstNumber}${secondNumber}`);
+}
+
+export default calculateCalibrationFromLine;
